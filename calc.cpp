e9ce1f7cecf25474144d94079c09755ac5abd232
Ubuntu <ashfaku@JavaMySQLProgram.fenqkdcuahnejkn4pji3daeorf.bx.internal.cloudapp.net>
2021-09-17 20:40:56 +0000

    wow

---
+++ calc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int x = 0, sum = 0;
+	char op;
+	std::cin >> x;
+	sum += x;
+	while (std::cin >> op)
+	{
+		std::cin >> x;
+		if (op == '+')
+			sum += x;
+		else if (op == '-')
+			sum -= x;
+	}
+	std::cout << sum << std::endl;
+	return 0;
+}
