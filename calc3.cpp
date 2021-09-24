0255e3ac439892262163e2910cb15007d67fa272
Ubuntu <ashfaku@JavaMySQLProgram.fenqkdcuahnejkn4pji3daeorf.bx.internal.cloudapp.net>
2021-09-24 13:25:04 +0000

    no

---
+++ calc3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::string s;
+	int sum = 0, input;
+	char op;
+	cin >> input;
+	char v = cin.peek();
+	if (v == '^')
+		input *= input;
+	sum += input;
+	cin >> op;
+	while (cin >> op)
+	{
+		if (op == ';')
+		{
+			cout << sum << endl;
+			cin >> input;
+			sum = input;
+		}
+		else
+		{
+			cin >> input;
+			char check = cin.peek();
+			bool c = (check == '^');
+			if (c)
+				input *= input;
+			if (op == '+')
+				sum += input;
+			else if (op == '-')
+				sum -= input;
+			if (c)
+				cin >> op;
+		}
+	}
+	return 0;
+}
