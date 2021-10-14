fd6d11fe145a01189ed034545797ea3d75d88aee
ashfaku <83651159+ashfaku@users.noreply.github.com>
2021-10-14 09:17:32 -0400

    Delete lab5 directory

--- lab5/funcs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-bool isDivisibleBy(int a, int b);
-bool isPrime(int x);
-int nextPrime(int z);
-int countPrimes(int a, int b);
-bool isTwinPrime(int x);
-int nextTwinPrime(int x);
-int largestTwinPrime(int a, int b);
--- lab5/tests.cpp
+++
@@ -1,9 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "funcs.h"
-
-TEST_CASE("Base Cases")
-{
-	CHECK(isPrime(-2) == false);
-	CHECK(isPrime(3) == true);
-}
